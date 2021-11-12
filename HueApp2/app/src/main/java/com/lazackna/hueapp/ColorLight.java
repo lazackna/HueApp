61cfec01e793a205ec2f417a2086d07c8e521012
jasper baijens <jasperbayens@outlook.com>
2021-11-12 19:33:34 +0100

    Dimmable lights dont have xy value and such. change light classes.

---
+++ HueApp2/app/src/main/java/com/lazackna/hueapp/ColorLight.java
@@ -0,0 +1,7 @@
+package com.lazackna.hueapp;
+
+public class ColorLight extends Light{
+    public ColorLight(int id, int bri, int hue, int sat, double[] xy, String name) {
+        super(id, bri, hue, sat, xy, name);
+    }
+}
---
+++ HueApp2/app/src/main/java/com/lazackna/hueapp/DimmableLight.java
@@ -0,0 +1,7 @@
+package com.lazackna.hueapp;
+
+public class DimmableLight extends Light{
+    public DimmableLight(int id, int bri, int hue, int sat, double[] xy, String name) {
+        super(id, bri, hue, sat, xy, name);
+    }
+}
