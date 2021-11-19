8e53a2c0eeb82ca350e7cd626a5c87848188744d
jasper baijens <jasperbayens@outlook.com>
2021-11-19 09:58:39 +0100

    Full colors.

--- HueApp2/app/src/main/java/com/lazackna/hueapp/Util/ColorHelper.java
+++ HueApp2/app/src/main/java/com/lazackna/hueapp/Util/ColorHelper.java
@@ -7,7 +7,7 @@ import androidx.annotation.IntRange;
 public class ColorHelper {
 
     public static int hueToColor(float hue, float sat, float val) {
-        return Color.HSVToColor(new float[] {(360f / 65535f) * hue,sat,val});
+        return Color.HSVToColor(new float[] {(360f / 65535f) * hue,(1f / 254f) * sat,(1f / 254f) * val});
         //(360 / hue.maxvalue) * hue
     }
 
